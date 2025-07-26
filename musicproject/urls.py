4386eba252b013a42861a7f93b04587d247e3cd0
Digambar Singh <singhdigambar266@gmail.com>
2025-07-26 18:32:45 +0530

    Edit project

--- musicproject/urls.py
+++ musicproject/urls.py
@@ -19,8 +19,10 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth.urls import views as auth_views
+from mymusic.views import root_redirect
 
 urlpatterns = [
+    path('', root_redirect),
     path('admin/', admin.site.urls),
     path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
